2d5e4b9ec3cb66788cd7dcc268ce9c6d7dfd27a1
Alexis1476 <halexisr20@hotmail.com>
2022-06-24 17:45:50 +0200

    Pygame initialization

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+import pygame
 from cell import *
 
 
 def main():
-    my_cell = MyCell('2')
+    is_running = True  # En cours d'execution
+    # Initialisation de pygame
+    pygame.init()
+    pygame.display.set_mode((400, 400))
+    while is_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                is_running = False
+
+    pygame.quit()
 
 
 if __name__ == '__main__':
