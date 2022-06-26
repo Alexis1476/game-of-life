0686627530042f68e0f14aa2fee09e6bf5382e78
Alexis1476 <halexisr20@hotmail.com>
2022-06-26 18:25:16 +0200

    Creation of constants

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import pygame
-from cell import *
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+WIDTH, HEIGHT = 700, 500
+SIZE = (WIDTH, HEIGHT)
 
 
 def main():
-    is_running = True  # En cours d'execution
-    # Initialisation de pygame
-    pygame.init()
-    pygame.display.set_mode((400, 400))
+    is_running = True  # En cours d'Execution
+    pygame.init()  # Initialisation de pygame
+    window = pygame.display.set_mode((400, 400))
+    window.fill(WHITE)
     while is_running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 is_running = False
-
+        pygame.display.flip()
     pygame.quit()
 
 
