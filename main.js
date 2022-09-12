11f4122f9a5db9413d747c95e1de396d46395d9b
Alexis1476 <halexisr20@hotmail.com>
2022-09-12 10:50:45 +0200

    Creation methode init qui initialise le tableau de cellules

--- main.js
+++ main.js
@@ -16,26 +16,28 @@ class Grid {
     constructor(canvas, width, height) {
         this.canvas = canvas;
         this.ctx = canvas.getContext('2d');
+        this.cells = [];
+
         canvas.width = width;
         canvas.height = height;
         canvas.style.border = 'solid 1px black'
-        //
-        this.cells = [];
-        let columns = width / Cell.width;
-        let rows = height / Cell.height;
+        this.init();
+    }
+    init() {
+        let columns = canvas.width / Cell.width;
+        let rows = canvas.height / Cell.height;
 
         for (let y = 0; y < rows; y++) {
             for (let x = 0; x < columns; x++) {
-                this.cells.push(new Cell(this.ctx, x,y));
+                this.cells.push(new Cell(this.ctx, x, y));
             }
-            
         }
     }
     draw() {
         this.cells.forEach(cell => {
             cell.draw();
             console.log(cell.x);
-            
+
         });
         console.log(this.cells.length);
     }
