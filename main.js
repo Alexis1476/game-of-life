209ff9614094aa9323d0ae6b6eb8006a41558154
Alexis1476 <halexisr20@hotmail.com>
2022-12-11 13:23:12 +0100

    Multidimensional array

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 class Cell {
     static width = 10;
     static height = 10;
+
     constructor(context, x, y) {
         this.ctx = context;
         this.x = x;
@@ -8,43 +9,43 @@ class Cell {
         // TODO : Faire méthode pour choisir le mode aléatoire
         this.isAlive = Math.random() > 0.5;
     }
+
     draw() {
-        let color;
-        color = this.isAlive ? '#000' : '#FFF';
-        this.ctx.fillStyle = color;
+        this.ctx.fillStyle = this.isAlive ? '#000' : '#FFF';
         this.ctx.fillRect(this.x * Cell.width, this.y * Cell.height, Cell.width, Cell.height);
     }
 }
 
 class Grid {
     constructor(canvas, width, height) {
-        this.canvas = canvas;
-        this.ctx = canvas.getContext('2d');
-        this.cells = [];
-
         canvas.width = width;
         canvas.height = height;
         canvas.style.border = 'solid 1px black'
+
+        this.ctx = canvas.getContext('2d');
+        this.columns = canvas.width / Cell.width;
+        this.rows = canvas.height / Cell.height;
+
+        this.cells = new Array(this.columns).fill(null).map(() =>
+            new Array(this.rows).fill(0));
+
         this.init();
     }
-    init() {
-        let columns = canvas.width / Cell.width;
-        let rows = canvas.height / Cell.height;
 
-        for (let y = 0; y < rows; y++) {
-            for (let x = 0; x < columns; x++) {
-                this.cells.push(new Cell(this.ctx, x, y));
+    init() {
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.columns; x++) {
+                this.cells[y][x] = (new Cell(this.ctx, x, y));
             }
         }
     }
+
     checkNeighbourhood() {
         // TODO : Mettre en place les règles du jeu
-
     }
+
     draw() {
-        this.cells.forEach(cell => {
-            cell.draw();
-        });
+        this.cells.map((row) => row.forEach(cell => cell.draw()));
     }
 }
 
